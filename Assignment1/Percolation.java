d760c7e9b7c3a8cabd44d8326a05c18cd5e0808d
JDylanM <dylanmdeveloper@gmail.com>
2017-06-21 15:45:18 +0200

    Progress

--- Assignment1/Percolation.java
+++ Assignment1/Percolation.java
@@ -13,10 +13,42 @@ public class Percolation {
     private int gridSize;
     private WeightedQuickUnionUF uf;
     private int openSites = 0;
+    private int topVirtualSite;
+    private int bottomVirtualSite;
+    private int totalSites;
+    
+    public Percolation(int n)
+    {
+        if(n <= 0) {
+            throw new IllegalArgumentException("n must be bigger than 0");
+        }
+        totalSites = n*n;
+        grid = new boolean[totalSites];
+        gridSize = n;
+        topVirtualSite = n*n;
+        bottomVirtualSite = n*n+1;
+        // plus 2 because of virtual top/bottom site.
+        uf = new WeightedQuickUnionUF(totalSites+2);
+    }
     
     public int numberOfOpenSites()
     {
         return openSites;
+    }
+    
+    private void connectToVirtualSites(int currentSite) {
+        // if top row, connect to virtual top site
+        if ( currentSite < gridSize ) {
+            uf.union(currentSite, topVirtualSite);
+        }
+        
+        System.out.println(currentSite):
+        
+        // same with bottom row
+        if ( currentSite > totalSites - gridSize ) {
+            System.out.println("i get hereee");
+            uf.union(currentSite, bottomVirtualSite);
+        }
     }
     
     private void connectToOpenNeighbors(int currentSite) 
@@ -25,6 +57,7 @@ public class Percolation {
         int leftOfSite = currentSite - 1;
         int rightOfSite = currentSite + 1;
         int belowSite = currentSite + gridSize;
+        int totalSites = gridSize*gridSize;
         
         if( aboveSite > 0 ) {
             if (grid[aboveSite]) {
@@ -32,7 +65,7 @@ public class Percolation {
             }
         }
         
-        if(rightOfSite % gridSize != 0 && rightOfSite < gridSize*gridSize) {
+        if(rightOfSite % gridSize != 0 && rightOfSite < totalSites) {
             if (grid[rightOfSite]) {
                 uf.union(currentSite, rightOfSite);
             }
@@ -44,12 +77,11 @@ public class Percolation {
             }
         }
         
-        if( belowSite < gridSize*gridSize ) {
+        if( belowSite < totalSites ) {
             if (grid[belowSite]) {
                 uf.union(currentSite, belowSite);
             }
         }
-            
     }
     
     private int xyTo1D(int row, int col)
@@ -64,23 +96,13 @@ public class Percolation {
         if (col <= 0 || col > gridSize) throw new IndexOutOfBoundsException("col index out of bounds");
     }
     
-    public Percolation(int n)
-    {
-        if(n <= 0) {
-            throw new IllegalArgumentException("n must be bigger than 0");
-        }
-        int totalSites = n*n;
-        grid = new boolean[totalSites];
-        gridSize = n;
-        uf = new WeightedQuickUnionUF(totalSites);
-    }
-    
     public void open(int row, int col)
     {
         validate(row, col);
         int oneDimensional = xyTo1D(row,col);
         grid[oneDimensional] = true;
         connectToOpenNeighbors(oneDimensional);
+        connectToVirtualSites(oneDimensional);
         openSites++;
     }
     
@@ -93,10 +115,9 @@ public class Percolation {
     public boolean isFull(int row, int col)
     {
         int oneDimensional = xyTo1D(row, col);
-        for(int topRowSite=0; topRowSite < gridSize; topRowSite++) {
-            if(uf.connected(oneDimensional, topRowSite)) {
-                return true;
-            }
+        int topVirtualSite = gridSize*gridSize;
+        if(uf.connected(oneDimensional, topVirtualSite)) {
+            return true;
         }
         return false;
     }
@@ -104,7 +125,7 @@ public class Percolation {
     
     public boolean percolates()
     {
-        return true;
+        return uf.connected(topVirtualSite, bottomVirtualSite);
     }
     
     public static void main(String[] args) 
@@ -115,12 +136,9 @@ public class Percolation {
         percolation.open(3, 2);
         percolation.open(4, 1);
         percolation.open(3, 1);
-        percolation.open(1,1);
-        System.out.println(percolation.uf.connected(10, 5));
-        System.out.println(percolation.uf.connected(10, 11));
-        System.out.println(percolation.uf.connected(10, 15));
-        System.out.println(percolation.uf.connected(10, 9));
+        percolation.open(5,1);
         System.out.println(percolation.isFull(3, 1));
+        System.out.println(percolation.percolates());
         
     }
 }
