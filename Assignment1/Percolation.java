0c70f535d5c47ff2754429d3b2d089386e8dcef3
JDylanM <dylanmdeveloper@gmail.com>
2017-06-21 15:51:26 +0200

    added visualizer from assignment and fixed percolation

--- Assignment1/Percolation.java
+++ Assignment1/Percolation.java
@@ -16,7 +16,7 @@ public class Percolation {
     private int topVirtualSite;
     private int bottomVirtualSite;
     private int totalSites;
-    
+
     public Percolation(int n)
     {
         if(n <= 0) {
@@ -30,72 +30,72 @@ public class Percolation {
         // plus 2 because of virtual top/bottom site.
         uf = new WeightedQuickUnionUF(totalSites+2);
     }
-    
+
     public int numberOfOpenSites()
     {
         return openSites;
     }
-    
+
     private void connectToVirtualSites(int currentSite) {
         // if top row, connect to virtual top site
         if ( currentSite < gridSize ) {
             uf.union(currentSite, topVirtualSite);
         }
-        
-        System.out.println(currentSite):
-        
+
+        System.out.println("currentSite " + Integer.toString(currentSite));
+        System.out.println("bigger Than " + Integer.toString(totalSites - gridSize));
+
         // same with bottom row
-        if ( currentSite > totalSites - gridSize ) {
-            System.out.println("i get hereee");
+        if ( currentSite >= totalSites - gridSize ) {
             uf.union(currentSite, bottomVirtualSite);
         }
     }
-    
-    private void connectToOpenNeighbors(int currentSite) 
+
+    private void connectToOpenNeighbors(int currentSite)
     {
         int aboveSite = currentSite - gridSize;
         int leftOfSite = currentSite - 1;
         int rightOfSite = currentSite + 1;
         int belowSite = currentSite + gridSize;
         int totalSites = gridSize*gridSize;
-        
+
         if( aboveSite > 0 ) {
             if (grid[aboveSite]) {
                 uf.union(currentSite, aboveSite);
             }
         }
-        
+
         if(rightOfSite % gridSize != 0 && rightOfSite < totalSites) {
             if (grid[rightOfSite]) {
                 uf.union(currentSite, rightOfSite);
             }
         }
-        
+
         if(leftOfSite % gridSize != gridSize-1 && leftOfSite > 0) {
             if (grid[leftOfSite]) {
                 uf.union(currentSite, leftOfSite);
             }
         }
-        
+
         if( belowSite < totalSites ) {
             if (grid[belowSite]) {
                 uf.union(currentSite, belowSite);
             }
         }
     }
-    
+
     private int xyTo1D(int row, int col)
     {
         validate(row, col);
         return (row-1) * gridSize + col-1;
     }
-    
+
     private void validate(int row, int col)
     {
         if (row <= 0 || row > gridSize) throw new IndexOutOfBoundsException("row index out of bounds");
         if (col <= 0 || col > gridSize) throw new IndexOutOfBoundsException("col index out of bounds");
     }
-    
+
     public void open(int row, int col)
     {
         validate(row, col);
@@ -105,13 +105,13 @@ public class Percolation {
         connectToVirtualSites(oneDimensional);
         openSites++;
     }
-    
+
     public boolean isOpen(int row, int col)
     {
         int oneDimensional = xyTo1D(row, col);
         return grid[oneDimensional];
     }
-    
+
     public boolean isFull(int row, int col)
     {
         int oneDimensional = xyTo1D(row, col);
@@ -121,24 +121,17 @@ public class Percolation {
         }
         return false;
     }
-    
-    
+
+
     public boolean percolates()
     {
         return uf.connected(topVirtualSite, bottomVirtualSite);
     }
-    
-    public static void main(String[] args) 
+
+    public static void main(String[] args)
     {
         Percolation percolation = new Percolation(5);
-        System.out.println(percolation.xyTo1D(3,1));
-        percolation.open(2, 1);
-        percolation.open(3, 2);
-        percolation.open(4, 1);
-        percolation.open(3, 1);
-        percolation.open(5,1);
-        System.out.println(percolation.isFull(3, 1));
         System.out.println(percolation.percolates());
-        
+
     }
 }
