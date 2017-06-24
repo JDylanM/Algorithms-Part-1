fd7373e3c67fcd3102f046a613993429b170b633
JDylanM <dylanmdeveloper@gmail.com>
2017-06-24 16:51:04 +0200

    hm en lösning på g

--- Assignment2/RandomizedQueue.java
+++ Assignment2/RandomizedQueue.java
@@ -6,11 +6,27 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     private int n;
 
     public RandomizedQueue() {
-
+        a = (Item[]) new Object[2];
+        n = 0;
     }
 
+    private void resize(int capacity) {
+        assert capacity >= n;
+
+        // textbook implementation
+        Item[] temp = (Item[]) new Object[capacity];
+        for (int i = 0; i < n; i++) {
+            temp[i] = a[i];
+        }
+        a = temp;
+
+       // alternative implementation
+       // a = java.util.Arrays.copyOf(a, capacity);
+    }
+
+
     public boolean isEmpty() {
-        return true;
+        return n == 0;
     }
 
     public int size() {
@@ -21,16 +37,23 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
     }
     public Item dequeue() {
-
+        if (isEmpty()) throw new NoSuchElementException("Stack underflow");
+        Item item = a[n-1];
+        a[n-1] = null;                              // to avoid loitering
+        n--;
+        // shrink size of array if necessary
+        if (n > 0 && n == a.length/4) resize(a.length/2);
+        return item;
     }
-    public Item sample() {
 
+    // need fix
+    public Item sample() {
+        return a[n-1]
     }
     public Iterator<Item> iterator() {
         return new RandomIterator;
     }
 
-    // an iterator, doesn't implement remove() since it's optional
     private class RandomIterator implements Iterator<Item> {
         private int i;
 
