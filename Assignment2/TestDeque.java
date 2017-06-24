57cbc1d9cea5734104bbf594b92a00f73eb35d66
JDylanM <dylanmdeveloper@gmail.com>
2017-06-24 15:31:47 +0200

    WOoooOoUUuuUUAuaau TestDeque seems to be working jao

--- Assignment2/TestDeque.java
+++ Assignment2/TestDeque.java
@@ -5,6 +5,54 @@ public class TestDeque {
     @Test
     public void testAddRemoveFirst() {
         Deque<Integer> deque = new Deque<>();
+        deque.addFirst(5);
+        deque.addFirst(4);
+        deque.addFirst(3);
+        deque.addFirst(2);
+        deque.addFirst(1);
+        assertEquals(1, (int) deque.removeFirst());
+        assertEquals(2, (int) deque.removeFirst());
+        assertEquals(3, (int) deque.removeFirst());
+        assertEquals(4, (int) deque.removeFirst());
+        assertEquals(5, (int) deque.removeFirst());
+    }
 
+    @Test
+    public void testAddRemoveLast() {
+        Deque<Integer> deque = new Deque<>();
+        deque.addLast(5);
+        deque.addLast(4);
+        deque.addLast(3);
+        deque.addLast(2);
+        deque.addLast(1);
+        assertEquals(1, (int) deque.removeLast());
+        assertEquals(2, (int) deque.removeLast());
+        assertEquals(3, (int) deque.removeLast());
+        assertEquals(4, (int) deque.removeLast());
+        assertEquals(5, (int) deque.removeLast());
+    }
+
+    @Test
+    public void testAddRemoveCombined() {
+        Deque<Integer> deque = new Deque<>();
+        deque.addLast(5);
+        deque.addLast(4);
+        deque.addFirst(1);
+        assertEquals(1, (int) deque.removeFirst());
+        assertEquals(5, (int) deque.removeFirst());
+        assertEquals(4, (int) deque.removeLast());
+    }
+
+    @Test
+    public void testEmpty() {
+        Deque<Integer> deque = new Deque<>();
+        deque.addLast(5);
+        deque.addLast(4);
+        deque.addFirst(1);
+        deque.removeFirst();
+        deque.removeFirst();
+        deque.removeFirst();
+        deque.addLast(5);
+        assertEquals(5, (int) deque.removeFirst());
     }
 }
