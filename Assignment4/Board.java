43012e57d35e3ef9fb68d3c2c1542fae3c310ad8
JDylanM <dylanmdeveloper@gmail.com>
2017-07-16 14:13:01 +0200

    clean manhattan and hamming code!

--- Assignment4/Board.java
+++ Assignment4/Board.java
@@ -1,16 +1,52 @@
-public class Board {
-    public Board(int[][] blocks) {
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+import java.lang.Math;
 
+public class Board {
+    private int[][] board;
+    private int manhattan = 0;
+
+    public Board(int[][] blocks) {
+        board = blocks;
+        computeManhattan();
     }
 
-    public int dimension() {
+    private void computeManhattan() {
+        int n = board.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int value = board[i][j];
+                int verticalDist = 0;
+                int horizontalDist = 0;
+                if(value != 0) {
+                    horizontalDist = Math.abs(j - (value - 1) % n);
+                    verticalDist = Math.abs(((value - 1) / n ) - i);
+                }
+                manhattan += verticalDist + horizontalDist;
+            }
+        }
+    }
+    public int dimension()
+ {
         return 0;
     }
     public int hamming() {
-        return 0;
+        int n = board.length;
+        int boardNumber = 1; // number from 1 -> n
+        int blocksInWrongPos = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int currentPos = board[i][j];
+                if (currentPos != boardNumber && currentPos != 0) {
+                    blocksInWrongPos++;
+                }
+                boardNumber++;
+            }
+        }
+        return blocksInWrongPos;
     }
     public int manhattan() {
-        return 0;
+        return manhattan;
     }
     public boolean isGoal() {
         return true;
@@ -30,6 +66,15 @@ public class Board {
     }
 
     public static void main(String[] args) {
-        System.out.println("tja");
+        // create initial board from file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] blocks = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                blocks[i][j] = in.readInt();
+        Board initial = new Board(blocks);
+        System.out.println(initial.hamming());
+        System.out.println(initial.manhattan());
     }
 }
