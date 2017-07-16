db7874f8580404e89a1d452b79a88211056b5377
JDylanM <dylanmdeveloper@gmail.com>
2017-07-16 12:10:56 +0200

    skeleton code

---
+++ Assignment4/Board.java
@@ -0,0 +1,35 @@
+public class Board {
+    public Board(int[][] blocks) {
+
+    }
+
+    public int dimension() {
+        return 0;
+    }
+    public int hamming() {
+        return 0;
+    }
+    public int manhattan() {
+        return 0;
+    }
+    public boolean isGoal() {
+        return true;
+    }
+    /*public Board twin() {
+
+    }*/
+    public boolean equals(Object y) {
+        return true;
+    }
+
+    /*public Iterable<Board> neighbors() {
+
+    }*/
+    public String toString() {
+        return "hej";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("tja");
+    }
+}
