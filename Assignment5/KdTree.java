208a51df8d23343526cb3979badeb5bfe98661b4
JDylanM <dylanmdeveloper@gmail.com>
2017-07-19 17:00:57 +0200

    commit
    bruteforce done

---
+++ Assignment5/KdTree.java
@@ -0,0 +1,68 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+import java.util.ArrayList;
+import edu.princeton.cs.algs4.SET;
+
+public class KdTree {
+    private SET<Point2D> set;
+    public KdTree() {
+        set = new SET<>();
+    }
+    public boolean isEmpty() {
+        return set.isEmpty();
+    }
+    public int size() {
+        return set.size();
+    }
+    public void insert(Point2D p) {
+        if (p == null) {
+            throw new IllegalArgumentException("argument is null");
+        }
+
+        set.add(p);
+    }
+    public boolean contains(Point2D p) {
+        if (p == null) {
+            throw new IllegalArgumentException("argument is null");
+        }
+
+        return true;
+    }
+    public void draw() {
+        for(Point2D p: set) {
+            p.draw();
+        }
+    }
+    public Iterable<Point2D> range(RectHV rect) {
+        if (rect == null) {
+            throw new IllegalArgumentException("argument is null");
+        }
+
+        ArrayList<Point2D> points = new ArrayList<>();
+
+        for(Point2D p: set) {
+            if (rect.contains(p)) {
+                points.add(p);
+            }
+        }
+
+        return points;
+    }
+
+    public Point2D nearest(Point2D p) {
+        double minDistance = p.distanceTo(set.max());
+        Point2D minPoint = set.max();
+        for (Point2D pointInSet: set) {
+            double distanceToPoint = p.distanceTo(pointInSet);
+            if  (minDistance > distanceToPoint ) {
+                minDistance = distanceToPoint;
+                minPoint = pointInSet;
+            }
+        }
+        return minPoint;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
--- Assignment5/PointSET.java
+++ Assignment5/PointSET.java
@@ -1,6 +1,6 @@
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
-import edu.princeton.cs.algs4.Stack;
+import java.util.ArrayList;
 import edu.princeton.cs.algs4.SET;
 
 public class PointSET {
@@ -9,26 +9,58 @@ public class PointSET {
         set = new SET<>();
     }
     public boolean isEmpty() {
-        return true;
+        return set.isEmpty();
     }
     public int size() {
-        return 0;
+        return set.size();
     }
     public void insert(Point2D p) {
+        if (p == null) {
+            throw new IllegalArgumentException("argument is null");
+        }
 
+        set.add(p);
     }
     public boolean contains(Point2D p) {
+        if (p == null) {
+            throw new IllegalArgumentException("argument is null");
+        }
+
         return true;
     }
     public void draw() {
-
+        for(Point2D p: set) {
+            p.draw();
+        }
     }
     public Iterable<Point2D> range(RectHV rect) {
-        return new Stack<Point2D>();
-    }
-    /*public Point2D nearest(Point2D p) {
+        if (rect == null) {
+            throw new IllegalArgumentException("argument is null");
+        }
 
-    }*/
+        ArrayList<Point2D> points = new ArrayList<>();
+
+        for(Point2D p: set) {
+            if (rect.contains(p)) {
+                points.add(p);
+            }
+        }
+
+        return points;
+    }
+
+    public Point2D nearest(Point2D p) {
+        double minDistance = p.distanceTo(set.max());
+        Point2D minPoint = set.max();
+        for (Point2D pointInSet: set) {
+            double distanceToPoint = p.distanceTo(pointInSet);
+            if  (minDistance > distanceToPoint ) {
+                minDistance = distanceToPoint;
+                minPoint = pointInSet;
+            }
+        }
+        return minPoint;
+    }
 
     public static void main(String[] args) {
 
