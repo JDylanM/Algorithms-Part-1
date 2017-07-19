96f43ef7021ce609d4165b3a02db030cc443883e
JDylanM <dylanmdeveloper@gmail.com>
2017-07-19 15:16:34 +0200

    Last assignment5 skeletoncode PointSet!

---
+++ Assignment5/PointSET.java
@@ -0,0 +1,36 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.SET;
+
+public class PointSET {
+    private SET<Point2D> set;
+    public PointSET() {
+        set = new SET<>();
+    }
+    public boolean isEmpty() {
+        return true;
+    }
+    public int size() {
+        return 0;
+    }
+    public void insert(Point2D p) {
+
+    }
+    public boolean contains(Point2D p) {
+        return true;
+    }
+    public void draw() {
+
+    }
+    public Iterable<Point2D> range(RectHV rect) {
+        return new Stack<Point2D>();
+    }
+    /*public Point2D nearest(Point2D p) {
+
+    }*/
+
+    public static void main(String[] args) {
+
+    }
+}
